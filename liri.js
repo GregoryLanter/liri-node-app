35b9ee4f7f58f92c3a2fed9982a6649940adae2d
Greg Lanter <dohnut22.gl@gmail.com>
2019-05-01 10:49:20 -0700

    made spotify run from command line input

--- liri.js
+++ liri.js
@@ -1,28 +1,55 @@
 var dotenv = require("dotenv");
 const result = dotenv.config();
- 
+
 if (result.error) {
-  console.log(result.error)
+    console.log(result.error)
 }
- 
-var keys = require("./keys.js");
-var axios = require("axios");
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
 
-userQuery = "walk this way";
-spotify.search({type: "track", query: userQuery}, function(err, data) {
-    if (err) {
-        console.log(err);
-    }
+var choice = process.argv[2].toLowerCase();
+var searchTerm = process.argv[3].toLowerCase();
 
-    var userSong = data.tracks.items;
-    var artist = "";
-    for(i=0; i<userSong.length;i++){
-        console.log("Artist: " + userSong[i].artists[0].name);
-        console.log("Song Name: " + userSong[i].name);
-        console.log("Preview Link: " + userSong[i].preview_url);
-        console.log("Album: " + userSong[i].album.name);
-        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    }
-});
+switch (choice) {
+    case "spotify-this-song":
+        spotify();
+        break;
+    case "movie-this":
+        omdb();
+        break;
+}
+function omdb() {
+var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=331ebb59";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Release Year: " + response.data.Year);
+  }
+);
+
+}
+
+function spotify() {
+    var keys = require("./keys.js");
+    var axios = require("axios");
+    var Spotify = require("node-spotify-api");
+    var spotify = new Spotify(keys.spotify);
+
+    userQuery = searchTerm;
+    spotify.search({ type: "track", query: userQuery }, function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+
+        var userSong = data.tracks.items;
+        var artist = "";
+        for (i = 0; i < userSong.length; i++) {
+            console.log("Artist: " + userSong[i].artists[0].name);
+            console.log("Song Name: " + userSong[i].name);
+            console.log("Preview Link: " + userSong[i].preview_url);
+            console.log("Album: " + userSong[i].album.name);
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        }
+    });
+}
